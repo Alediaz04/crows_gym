b99990a4220afd5f2cc14222d7ebdae1ab3cc649
Alejandro <alejandrodanieldiaz2004@gmail.com>
2024-10-31 21:07:25 -0300

    Logica aplicada a segunda pregunta

--- trivia.js
+++ trivia.js
@@ -1,5 +1,22 @@
 function quiz1(ques, opc, btn){
+    const buttons = document.querySelectorAll('.btn1');
+    buttons.forEach(button => button.disabled = true);
+    
     if(ques===1 && opc===2){
+       btn.setAttribute("class", "correcto");
+        
+
+    }else{
+       
+        btn.setAttribute("class", "incorrecto");
+      
+    }
+}
+function quiz2(ques, opc, btn){
+    const buttons = document.querySelectorAll('.btn2');
+    buttons.forEach(button => button.disabled = true);
+
+    if(ques===1 && opc===3){
        btn.setAttribute("class", "correcto");
         
 
